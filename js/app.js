341b4bdd7f809c224751fe7544210db98faca4b9
phanindrakoka09 <phanindrakoka@gmail.com>
2018-09-05 16:22:50 +0530

    complete code for app.js file

--- js/app.js
+++ js/app.js
@@ -41,6 +41,90 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (x, y) {
+
+    // Variables for the player to move along x and y axis 
+    this.x = x;
+    this.y = y;
+
+    //The image of the player of horn-girl is added to the playing field 
+    this.player = 'images/char-horn-girl.png';
+};
+
+Player.prototype.update = function (dt) {
+
+};
+
+// Renders the image of the user into the game
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.player), this.x, this.y);
+};
+
+// Allows the user to use the arrow keys to jump from tile to tile
+Player.prototype.handleInput = function (keyPress) {
+
+    // Enables user on left arrow key to move left on the x axis by 102
+    // Also enables user not to go off the game tiles on the left side
+    if (keyPress == 'left' && this.x > 0) {
+        this.x -= 102;
+    };
+
+    // Enables user on right arrow key to move right on the x axis by 102
+    // Also enables user not to go off the game tiles on the right side
+    if (keyPress == 'right' && this.x < 405) {
+        this.x += 102;
+    };
+
+    // Enables user on up arrow key to move upwards on the y axis by 83
+    if (keyPress == 'up' && this.y > 0) {
+        this.y -= 83;
+    };
+
+    // Enables user on down arrow key to move downwards on the y axis by 83
+    // Also enables user not to go off the game tiles on the bottom side
+    if (keyPress == 'down' && this.y < 405) {
+        this.y += 83;
+    };
+
+    // Once the user reaches the top of the page; the water, the user is
+    // Instantly reset to the starting position
+    if (this.y < 0) {
+        setTimeout(() => {
+            this.x = 202;
+            this.y = 405;
+        }, 800);
+    };
+};
+
+
+// All enemies are placed in an array
+var allEnemies = [];
+
+// Location of the 3 enemies on the y axis located on the stone road
+var enemyLocation = [63, 147, 230];
+
+
+// For each enemy located on the y axis from 0 on the x axis move at a speed of 200 
+// Until randomly regenerated in the enemy update function above
+enemyLocation.forEach(function (locationY) {
+    enemy = new Enemy(0, locationY, 200);
+    allEnemies.push(enemy);
+});
+
+// The starting location of the player is located at x=200, y=405
+var player = new Player(202, 405);
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. 
+document.addEventListener('keyup', function (e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+    player.handleInput(allowedKeys[e.keyCode]);
+});
 
 
 // Now instantiate your objects.
