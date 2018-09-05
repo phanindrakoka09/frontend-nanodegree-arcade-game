176e19df0dc9ce050d5ea002a98a184dfd8a15c8
phanindrakoka09 <phanindrakoka@gmail.com>
2018-09-05 16:31:04 +0530

    edited the comments and checked the overall code for bugs

--- js/app.js
+++ js/app.js
@@ -1,46 +1,44 @@
 // Enemies our player must avoid
-var Enemy = function(x, y , speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-        this.x = x;
-        this.y = y;
-        this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function (x, y, speed) {
+
+    // The following variables are used to determine the x and y axis and speed of the enemy
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+
+    // The image of the enemy cockroach is added to the playing field 
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
+// Update the enemy's position, required for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
 
-//this will multiply the speed using dt parameter on x-axis
+    // Multiplies the speed by the dt parameter on the x axis
     this.x += this.speed * dt;
-    // You should multiply any movement by the dt parameter
-    if (this.x > 510){
+
+    // Once enemies are off the canvas, they reappear randomly with different speeds
+    if (this.x > 510) {
         this.x = -50;
         this.speed = 100 + Math.floor(Math.random() * 222);
-    }
-    // check for collisions between player and enemy
+    };
+
+    // Checks for collisions between the player and enemies
     if (player.x < this.x + 80 &&
         player.x + 80 > this.x &&
         player.y < this.y + 60 &&
-        60+ player.y > this.y) {
+        60 + player.y > this.y) {
         player.x = 202;
-        player.y = 405
+        player.y = 405;
     };
-    // which will ensure the game runs at the same speed for
-    // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+// Renders the enemy into the game
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class focusing on x and y axis
 var Player = function (x, y) {
 
     // Variables for the player to move along x and y axis 
@@ -123,25 +121,5 @@ document.addEventListener('keyup', function (e) {
         39: 'right',
         40: 'down'
     };
-    player.handleInput(allowedKeys[e.keyCode]);
-});
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
