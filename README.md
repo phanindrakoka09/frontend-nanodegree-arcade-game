72477149637c26bb1c69a46c4f029ea7254a6c93
phanindrakoka09 <phanindrakoka@gmail.com>
2018-09-06 23:48:18 +0530

    made the requested changes to README.md file

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ frontend-nanodegree-arcade-game
 Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
 
 For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+===============================
+Please refer the guide below to know how to play the game !
+===============================
+-->First open the index.html file in your favourite browser by double-clicking on it.
+
+-->Then you will see the main character standing on the green tile and few enemies roaming around.
+
+-->The objective of the game is to bring the character to the blue tiles present at the top of the page so that you win.
+-->Use the arrow keys up , down , left & right to control the character.
+
+-->Avoid colliding with enemy bugs so that you will not loose if by chance you collide with one you will end up starting the game again from first.
+
+-->These are the simple rules enjoy the game.
--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ var Enemy = function (x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
+
 // Update the enemy's position, required for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function (dt) {
@@ -21,7 +22,7 @@ Enemy.prototype.update = function (dt) {
     if (this.x > 510) {
         this.x = -50;
         this.speed = 100 + Math.floor(Math.random() * 222);
-    };
+    }
 
     // Checks for collisions between the player and enemies
     if (player.x < this.x + 80 &&
@@ -30,7 +31,7 @@ Enemy.prototype.update = function (dt) {
         60 + player.y > this.y) {
         player.x = 202;
         player.y = 405;
-    };
+    }
 };
 
 // Renders the enemy into the game
@@ -65,24 +66,24 @@ Player.prototype.handleInput = function (keyPress) {
     // Also enables user not to go off the game tiles on the left side
     if (keyPress == 'left' && this.x > 0) {
         this.x -= 102;
-    };
+    }
 
     // Enables user on right arrow key to move right on the x axis by 102
     // Also enables user not to go off the game tiles on the right side
     if (keyPress == 'right' && this.x < 405) {
         this.x += 102;
-    };
+    }
 
     // Enables user on up arrow key to move upwards on the y axis by 83
     if (keyPress == 'up' && this.y > 0) {
         this.y -= 83;
-    };
+    }
 
     // Enables user on down arrow key to move downwards on the y axis by 83
     // Also enables user not to go off the game tiles on the bottom side
     if (keyPress == 'down' && this.y < 405) {
         this.y += 83;
-    };
+    }
 
     // Once the user reaches the top of the page; the water, the user is
     // Instantly reset to the starting position
@@ -91,7 +92,7 @@ Player.prototype.handleInput = function (keyPress) {
             this.x = 202;
             this.y = 405;
         }, 800);
-    };
+    }
 };
 
 
